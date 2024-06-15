6c7f1850e21d7b24a654725d0b5872871979a4ab
Tara Denniston <taradenniston@gmail.com>
2024-06-15 10:17:11 -0400

    update POST route to add data validation

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const Book = require("../models/book");
-
 const router = new express.Router();
+const ExpressError = require("../expressError")
+const jsonschema = require("jsonschema");
+const bookSchema = require("../schemas/bookSchema.json");
 
 
 /** GET / => {books: [book, ...]}  */
@@ -30,6 +32,11 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, bookSchema);
+    if (!result.valid) {
+      let listOfErrors = result.errors.map(error => error.stack);
+      throw new ExpressError(listOfErrors, 400);
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
